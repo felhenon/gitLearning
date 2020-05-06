0382657925d3e0052a98540357d184c47b0d1f66
felhenon <huseyingursoy@hotmail.com>
2020-05-06 17:29:56 +0200

    Spring.java

--- src/tests/Login.java
+++ src/tests/Login.java
@@ -3,7 +3,7 @@ package tests;
 public class Login {
     public static void main(String[] args) {
 
-        String one = "Hello";
+        String one = "Hello World";
         System.out.println("Merhaba = " + one);
     }
 }
