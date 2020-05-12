4898e94a447e8288353cf3a3850aa3db052a9111
felhenon <62045954+felhenon@users.noreply.github.com>
2020-05-12 17:08:04 +0200

    Update Practice.java

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -2,7 +2,7 @@ package tests;
 
 public class Practice {
     public static void main(String[] args) {
-    System.out.println("these lines added into github"){
-    }
+    System.out.println("these lines added into github");
+    
     }
 }
